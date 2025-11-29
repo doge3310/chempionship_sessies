12c7e4fc6a376d74f99123d5937bdbdf77022474
doge3310 <dk9687377@gmail.com>
2025-11-29 20:04:50 +0300

    sql insert and init db

---
+++ WorkingCalendar.mysql.sql
@@ -0,0 +1,27 @@
+CREATE TABLE WorkingCalendar
+(
+    Id            INT PRIMARY KEY,
+    ExceptionDate DATE       NOT NULL,
+    IsWorkingDay  TINYINT(1) NOT NULL
+);
+
+INSERT INTO WorkingCalendar (Id, ExceptionDate, IsWorkingDay) VALUES (1, '2024-01-01', 0);
+INSERT INTO WorkingCalendar (Id, ExceptionDate, IsWorkingDay) VALUES (2, '2024-01-02', 0);
+INSERT INTO WorkingCalendar (Id, ExceptionDate, IsWorkingDay) VALUES (3, '2024-01-03', 0);
+INSERT INTO WorkingCalendar (Id, ExceptionDate, IsWorkingDay) VALUES (4, '2024-01-04', 0);
+INSERT INTO WorkingCalendar (Id, ExceptionDate, IsWorkingDay) VALUES (5, '2024-01-05', 0);
+INSERT INTO WorkingCalendar (Id, ExceptionDate, IsWorkingDay) VALUES (6, '2024-01-08', 0);
+INSERT INTO WorkingCalendar (Id, ExceptionDate, IsWorkingDay) VALUES (7, '2024-02-23', 0);
+INSERT INTO WorkingCalendar (Id, ExceptionDate, IsWorkingDay) VALUES (8, '2024-03-08', 0);
+INSERT INTO WorkingCalendar (Id, ExceptionDate, IsWorkingDay) VALUES (9, '2024-04-27', 1);
+INSERT INTO WorkingCalendar (Id, ExceptionDate, IsWorkingDay) VALUES (10, '2024-04-29', 0);
+INSERT INTO WorkingCalendar (Id, ExceptionDate, IsWorkingDay) VALUES (11, '2024-04-30', 0);
+INSERT INTO WorkingCalendar (Id, ExceptionDate, IsWorkingDay) VALUES (12, '2024-05-01', 0);
+INSERT INTO WorkingCalendar (Id, ExceptionDate, IsWorkingDay) VALUES (13, '2024-05-09', 0);
+INSERT INTO WorkingCalendar (Id, ExceptionDate, IsWorkingDay) VALUES (14, '2024-05-10', 0);
+INSERT INTO WorkingCalendar (Id, ExceptionDate, IsWorkingDay) VALUES (15, '2024-06-12', 0);
+INSERT INTO WorkingCalendar (Id, ExceptionDate, IsWorkingDay) VALUES (16, '2024-11-02', 1);
+INSERT INTO WorkingCalendar (Id, ExceptionDate, IsWorkingDay) VALUES (17, '2024-11-04', 0);
+INSERT INTO WorkingCalendar (Id, ExceptionDate, IsWorkingDay) VALUES (18, '2024-12-28', 1);
+INSERT INTO WorkingCalendar (Id, ExceptionDate, IsWorkingDay) VALUES (19, '2024-12-30', 0);
+INSERT INTO WorkingCalendar (Id, ExceptionDate, IsWorkingDay) VALUES (20, '2024-12-31', 0);
